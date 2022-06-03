63320781bbd526fc51abfb53b4f0073a36e07a56
Gonzalo Gomez <gonzaloholzmeister@gmail.com>
2022-06-03 09:34:41 -0300

    Form render

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-// Code goes here!
+class ProjectInput {
+    protected templateEl: HTMLTemplateElement; 
+    protected hostEl: HTMLDivElement; 
+    protected formEl: HTMLFormElement;
+
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        this.loadDOMElements();
+    }
+    
+    private loadDOMElements() {
+        this.templateEl = <HTMLTemplateElement> document.getElementById('project-input')!;
+        this.hostEl = <HTMLDivElement> document.getElementById('app')!;
+
+        const importedNode = document.importNode(this.templateEl.content, true);
+        this.formEl = <HTMLFormElement> importedNode.firstElementChild;
+        this.hostEl.insertAdjacentElement('afterbegin', this.formEl);
+    }
+}
+
+const project = new ProjectInput();
